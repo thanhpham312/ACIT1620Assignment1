079cfe910dcde5e9da58ab15bc3f0be21e7ea264
thanhpham312 <tpham74@my.bcit.ca>
2017-10-07 18:06:53 -0700

    Cleaned up javaScript.

--- javaScript.js
+++ javaScript.js
@@ -22,10 +22,12 @@ var buttonReset = document.getElementById("buttonReset");
 var controlPanel = document.getElementById("controlPanel");
 var controlPanelMisc = document.getElementById("controlPanelMisc");
 
-var imgList1 = ["rm", "rm1.png", "rm2.png", "rm3.png"];
-var imgList2 = ["ar", "ar1.png", "ar2.png", "ar3.png"];
-var imgList3 = ["bj", "bj1.png", "bj2.png", "bj3.png"];
-var imgList4 = ["fr", "fr1.png", "fr2.png", "fr3.png"];
+var imgList1 = ["rm1.png", "rm2.png", "rm3.png"];
+var imgList2 = ["ar1.png", "ar2.png", "ar3.png"];
+var imgList3 = ["bj1.png", "bj2.png", "bj3.png"];
+var imgList4 = ["fr1.png", "fr2.png", "fr3.png"];
+
+var selectedImg = topImg;
 
 
 if (typeof currentImgList === "undefined") {
@@ -35,177 +37,68 @@ if (typeof currentImgList === "undefined") {
 // Define functions:
 
 function displayImg() {
-    if(currentImgList[0] == "rm") {
-        topImg.style.boxShadow = "0px 0px 0px 5px rgba(2, 168, 243, 0.5)";
-        bottomImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
-        leftImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
-        rightImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
-    }
-    else if(currentImgList[0] == "ar") {
-        bottomImg.style.boxShadow = "0px 0px 0px 5px rgba(2, 168, 243, 0.5)";
-        topImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
-        leftImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
-        rightImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
-    }
-    else if(currentImgList[0] == "bj") {
-        leftImg.style.boxShadow = "0px 0px 0px 5px rgba(2, 168, 243, 0.5)";
-        topImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
-        bottomImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
-        rightImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
-    }
-    else if(currentImgList[0] == "fr") {
-        rightImg.style.boxShadow = "0px 0px 0px 5px rgba(2, 168, 243, 0.5)";
-        topImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
-        bottomImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
-        leftImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
-    }
-    thumbImg1.style.backgroundImage = "url(img/" + currentImgList[1] +  ")";
-    thumbImg2.style.backgroundImage = "url(img/" + currentImgList[2] +  ")";
-    thumbImg3.style.backgroundImage = "url(img/" + currentImgList[3] +  ")";
+    topImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
+    bottomImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
+    leftImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
+    rightImg.style.boxShadow = "0px 0px 0px 5px rgba(255, 255, 255, 0.8)";
+
+    selectedImg.style.boxShadow = "0px 0px 0px 5px rgba(2, 168, 243, 0.5)";
+
+    thumbImg1.style.backgroundImage = "url(img/" + currentImgList[0] +  ")";
+    thumbImg2.style.backgroundImage = "url(img/" + currentImgList[1] +  ")";
+    thumbImg3.style.backgroundImage = "url(img/" + currentImgList[2] +  ")";
 }
 
 function changeFocus(imgNum) {
     if(imgNum == 1) {
-        currentImgList = imgList1;
+        selectedImg = topImg;
+        currentImgList = imgList1.slice();
         displayImg();
     }
     else if(imgNum == 2) {
-        currentImgList = imgList2;
+        selectedImg = bottomImg;
+        currentImgList = imgList2.slice();
         displayImg();
     }
     else if(imgNum == 3) {
-        currentImgList = imgList3;
+        selectedImg = leftImg;
+        currentImgList = imgList3.slice();
         displayImg();
     }
     else if(imgNum == 4) {
-        currentImgList = imgList4;
+        selectedImg = rightImg;
+        currentImgList = imgList4.slice();
         displayImg();
     }
 }
 
 function showClickedImg(thumbNum) {
-    if(currentImgList[0] == "rm") {
-        topImg.style.backgroundImage = "url(img/" + currentImgList[thumbNum] +  ")";
-    }
-    else if(currentImgList[0] == "ar") {
-        bottomImg.style.backgroundImage = "url(img/" + currentImgList[thumbNum] +  ")";
-    }
-    else if(currentImgList[0] == "bj") {
-        leftImg.style.backgroundImage = "url(img/" + currentImgList[thumbNum] +  ")";
-    }
-    else if(currentImgList[0] == "fr") {
-        rightImg.style.backgroundImage = "url(img/" + currentImgList[thumbNum] +  ")";
-    }
+    selectedImg.style.backgroundImage = "url(img/" + currentImgList[thumbNum] +  ")";
 }
 
 function moveImg(moveNum) {
-    if(currentImgList[0] == "rm") {
-        if(moveNum == 1) {
-            topImg.style.left = topImg.offsetLeft + 120 - 30 + "px";
-        }
-        else if(moveNum == 2) {
-            topImg.style.left = topImg.offsetLeft + 120 + 30 + "px";
-        }
-        else if(moveNum == 3) {
-            topImg.style.top = topImg.offsetTop + 67.5 - 30 + "px";
-        }
-        else if(moveNum == 4) {
-            topImg.style.top = topImg.offsetTop + 67.5 + 30 + "px";
-        }
+    if(moveNum == 1) {
+        selectedImg.style.left = selectedImg.offsetLeft + 120 - 30 + "px";
     }
-    else if(currentImgList[0] == "ar") {
-        if(moveNum == 1) {
-            bottomImg.style.left = bottomImg.offsetLeft + 120 - 30 + "px";
-        }
-        else if(moveNum == 2) {
-            bottomImg.style.left = bottomImg.offsetLeft + 120 + 30 + "px";
-        }
-        else if(moveNum == 3) {
-            bottomImg.style.top = bottomImg.offsetTop + 67.5 - 30 + "px";
-        }
-        else if(moveNum == 4) {
-            bottomImg.style.top = bottomImg.offsetTop + 67.5 + 30 + "px";
-        }
+    else if(moveNum == 2) {
+        selectedImg.style.left = selectedImg.offsetLeft + 120 + 30 + "px";
     }
-    else if(currentImgList[0] == "bj") {
-        if(moveNum == 1) {
-            leftImg.style.left = leftImg.offsetLeft + 120 - 30 + "px";
-        }
-        else if(moveNum == 2) {
-            leftImg.style.left = leftImg.offsetLeft + 120 + 30 + "px";
-        }
-        else if(moveNum == 3) {
-            leftImg.style.top = leftImg.offsetTop + 67.5 - 30 + "px";
-        }
-        else if(moveNum == 4) {
-            leftImg.style.top = leftImg.offsetTop + 67.5 + 30 + "px";
-        }
+    else if(moveNum == 3) {
+        selectedImg.style.top = selectedImg.offsetTop + 67.5 - 30 + "px";
     }
-    else if(currentImgList[0] == "fr") {
-        if(moveNum == 1) {
-            rightImg.style.left = rightImg.offsetLeft + 120 - 30 + "px";
-        }
-        else if(moveNum == 2) {
-            rightImg.style.left = rightImg.offsetLeft + 120 + 30 + "px";
-        }
-        else if(moveNum == 3) {
-            rightImg.style.top = rightImg.offsetTop + 67.5 - 30 + "px";
-        }
-        else if(moveNum == 4) {
-            rightImg.style.top = rightImg.offsetTop + 67.5 + 30 + "px";
-        }
+    else if(moveNum == 4) {
+        selectedImg.style.top = selectedImg.offsetTop + 67.5 + 30 + "px";
     }
-    /*else if(currentImgList[0] == "ar") {
-        bottomImg.style.backgroundImage = "url(img/" + currentImgList[thumbNum] +  ")";
-    }
-    else if(currentImgList[0] == "bj") {
-        leftImg.style.backgroundImage = "url(img/" + currentImgList[thumbNum] +  ")";
-    }
-    else if(currentImgList[0] == "fr") {
-        rightImg.style.backgroundImage = "url(img/" + currentImgList[thumbNum] +  ")";
-    }*/
 }
 
 function changeImgSize(visibilityNum) {
-    if(currentImgList[0] == "rm") {
-        if(visibilityNum == 0) {
-            topImg.style.width = topImg.offsetWidth*0.9 + "px";
-            topImg.style.height = topImg.offsetHeight*0.9 + "px";
-        }
-        else if(visibilityNum == 1) {
-            topImg.style.width = topImg.offsetWidth*1.1 + "px";
-            topImg.style.height = topImg.offsetHeight*1.1 + "px";
-        }
+    if(visibilityNum == 0) {
+        selectedImg.style.width = selectedImg.offsetWidth*0.9 + "px";
+        selectedImg.style.height = selectedImg.offsetHeight*0.9 + "px";
     }
-    else if(currentImgList[0] == "ar") {
-        if(visibilityNum == 0) {
-            bottomImg.style.width = bottomImg.offsetWidth*0.9 + "px";
-            bottomImg.style.height = bottomImg.offsetHeight*0.9 + "px";
-        }
-        else if(visibilityNum == 1) {
-            bottomImg.style.width = bottomImg.offsetWidth*1.1 + "px";
-            bottomImg.style.height = bottomImg.offsetHeight*1.1 + "px";
-        }
-    }
-    else if(currentImgList[0] == "bj") {
-        if(visibilityNum == 0) {
-            leftImg.style.width = leftImg.offsetWidth*0.9 + "px";
-            leftImg.style.height = leftImg.offsetHeight*0.9 + "px";
-        }
-        else if(visibilityNum == 1) {
-            leftImg.style.width = leftImg.offsetWidth*1.1 + "px";
-            leftImg.style.height = leftImg.offsetHeight*1.1 + "px";
-        }
-    }
-    else if(currentImgList[0] == "fr") {
-        if(visibilityNum == 0) {
-            rightImg.style.width = rightImg.offsetWidth*0.9 + "px";
-            rightImg.style.height = rightImg.offsetHeight*0.9 + "px";
-        }
-        else if(visibilityNum == 1) {
-            rightImg.style.width = rightImg.offsetWidth*1.1 + "px";
-            rightImg.style.height = rightImg.offsetHeight*1.1 + "px";
-        }
+    else if(visibilityNum == 1) {
+        selectedImg.style.width = selectedImg.offsetWidth*1.1 + "px";
+        selectedImg.style.height = selectedImg.offsetHeight*1.1 + "px";
     }
 }
 
@@ -221,18 +114,7 @@ function controlPanelVisibility(sizeNum) {
 }
 
 function resetImg() {
-    if(currentImgList[0] == "rm") {
-        topImg.removeAttribute("style")
-    }
-    else if(currentImgList[0] == "ar") {
-        bottomImg.removeAttribute("style")
-    }
-    else if(currentImgList[0] == "bj") {
-        leftImg.removeAttribute("style")
-    }
-    else if(currentImgList[0] == "fr") {
-        rightImg.removeAttribute("style")
-    }
+    selectedImg.removeAttribute("style");
     displayImg();
 }
 
@@ -261,15 +143,15 @@ rightImg.addEventListener("click", function() {
 // Change display image on the focused window:
 
 thumbImg1.addEventListener("click", function() {
-    showClickedImg(1);
+    showClickedImg(0);
 })
 
 thumbImg2.addEventListener("click", function() {
-    showClickedImg(2);
+    showClickedImg(1);
 })
 
 thumbImg3.addEventListener("click", function() {
-    showClickedImg(3);
+    showClickedImg(2);
 })
 
 // Move focused window:
